9413f1b8262eeaf35cd86e6ccd380110d5c0b3a6
Deepak K Anand <deepak.kanand@ymail.com>
2016-01-26 14:19:53 +0530

    Corrected the Indentation

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 var config = {
-	"orgName" 			: "Dazeworks",
-	"defRouterDomain" 	: "http://192.168.1.1:8090/**",
-	"defRouterEP" 		: "http://192.168.1.1:8090/httpclient.html"
+    "orgName"           : "Dazeworks",
+    "defRouterDomain"   : "http://192.168.1.1:8090/**",
+    "defRouterEP"       : "http://192.168.1.1:8090/httpclient.html"
 };
